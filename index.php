db996f018d73f5fed98ad8d308ee0f50d35e4fe3
Mykhailo.Tsaruk <mykhailo.tsaruk@student.tuke.sk>
2022-11-18 18:15:39 +0100

    test base form

--- index.php
+++ index.php
@@ -12,8 +12,24 @@
             <div class="col-md-6 offset-md-3">
                 <form>
                     <div class="form-group">
-                        <label for="Your name">Name</label>
+                        <label for="name">Your Name</label>
                         <input type="name" class="form-control" id="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="surname"> </label>
+                        <input type="surname" class="form-control" id="surname">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">E-mail</label>
+                        <input type="email" class="form-control" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="phone">Phone number</label>
+                        <input type="phone" class="form-control" id="phone">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="Password">
                     </div>
                 </form>
             </div>
