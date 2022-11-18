fea1a6539bca0c3db0580ddc9cd07d78bae9b8f9
Mykhailo.Tsaruk <mykhailo.tsaruk@student.tuke.sk>
2022-11-18 18:42:07 +0100

    test base form

---
+++ WebAzure/registration.php
@@ -0,0 +1,30 @@
+<?php
+
+    $data = [
+        'name' => [
+            'field_name' => 'Name',
+            'requiered'  => true,
+        ],
+        'surname' => [
+            'field_name' => 'Surname',
+            'requiered'  => false,
+        ],
+        'email' => [
+            'field_name' => 'Email',
+            'requiered'  => true,
+        ],
+        'phone' => [
+            'field_name' => 'Phone number',
+            'requiered'  => false,
+        ],
+        'password' => [
+            'field_name' => 'Password',
+            'requiered'  => true,
+        ],        
+        'repeat-password' => [
+            'field_name' => 'Repeat password',
+            'requiered'  => true,
+        ]
+    ];
+
+?>
