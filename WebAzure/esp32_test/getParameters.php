5f20f933b1a69f891de068c42b1adef3b10843bf
Mykhailo.Tsaruk <mykhailo.tsaruk@student.tuke.sk>
2022-12-16 21:23:42 +0100

    test esp32

--- WebAzure/esp32_test/getParameters.php
+++ WebAzure/esp32_test/getParameters.php
@@ -10,4 +10,10 @@ $sum = $sn1 + $sn2;
 echo $text;
 echo "<br>";
 echo "Sum:" . $sum;
+
+$file1 = fopen("sensors.txt","w") or die("Unable to open file!");
+$text1 = "a=" . $sn1 . " b=" . $sn2;
+
+fwrite($file1, $text1);
+fclose($file1);
 ?>
