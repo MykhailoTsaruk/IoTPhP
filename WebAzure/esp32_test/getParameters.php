eaa06ae79025bcf65d91b2e37b3f6d9b35ead2da
Mykhailo.Tsaruk <mykhailo.tsaruk@student.tuke.sk>
2022-12-22 04:20:37 +0100

    update

--- WebAzure/esp32_test/getParameters.php
+++ WebAzure/esp32_test/getParameters.php
@@ -1,18 +1,17 @@
 <?php
 echo '<h1>getParameters page</h1>';
 
-$sn1 = $_GET["a"];
-$sn2 = $_GET["b"];
+$sn1 = $_GET["balance"];
 
-$text = "a=" . $sn1 . " b=" . $sn2;
-$sum = $sn1 + $sn2;
+$text = "balance=" . $sn1;
+$sum = $sn1;
 
 echo $text;
 echo "<br>";
 echo "Sum:" . $sum;
 
 $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
-$text1 = "a=" . $sn1 . " b=" . $sn2;
+$text1 = "a=" . $sn1;
 
 fwrite($file1, $text1);
 fclose($file1);
