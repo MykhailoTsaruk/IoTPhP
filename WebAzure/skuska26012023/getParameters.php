b970da1e8d25e2431b2a53723644ecf1a8371cdb
Mykhailo Tsaruk <mykhailo.tsaruk@student.tuke.sk>
2023-01-26 09:36:16 +0100

    add skuska

--- WebAzure/skuska26012023/getParameters.php
+++ WebAzure/skuska26012023/getParameters.php
@@ -6,5 +6,11 @@ $sn2 = $_GET["humidity"];
 $text = "temperature = " . $sn1 . " °C" . " humidity = " . $sn2 . " %";
 
 echo $text;
+echo "<br>";
 
+$file1 = fopen("sensor.txt","w") or die("Unable to open file!");
+$text1 = "temperature = " . $sn1 . " °C" . " humidity = " . $sn2 . " %";
+
+fwrite($file1, $text1);
+fclose($file1);
 ?>
